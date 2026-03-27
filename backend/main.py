60f74aaca5a408a4c662b96c421639df4556f4a1
rachidedndev <lucas_rachide92@hotmail.com>
2026-03-27 17:33:00 -0300

    fix: correcting rout

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://rpgenthings.vercel.app/"
+        "https://rpgenthings.vercel.app",
         ],
     allow_credentials=True,
     allow_methods=["*"],
